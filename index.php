2f58bb443aa8879f364cc4ad05dba8a5302ac251
imane ELKADI <imaneelkadi22@gmail.com>
2023-04-01 14:41:32 +0000

    add new

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello girls</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+    <div class="d1">
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="">Home</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!-- <input type="text" placholder="type something..." class="i1"> -->
+<textarea class="i1" name="story"
+         >
+
+</textarea>
+<!-- <h1 class="h1">Welcome zombies's lovers</h1> -->
+
+    </div>
+    <div class="d2">
+
+    </div>
+    <div class="d3">
+
+    <img src="image.jpg" class="im1">
+    <!-- </div>
+    <div class="loader">
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+</div> -->
+
+<div class="subscribe">
+    <p>SUBSCRIBE</p>
+    <input placeholder="Your e-mail" class="subscribe-input" name="email" type="email">
+    <br>
+    <div class="submit-btn">SUBMIT</div>
+  </div>
+<!-- <p class="p1">Website is a hyper-media information storage system linking resources
+     around the world. In website, browsers allow highlighted words or an icon
+      which is called hyper links to display text, video, graphics and sound on a
+       local computer screen. With the introduction of World Wide Web in 1989, 
+       the concept of websites has become more popular. 
+       On this web, anyone can create a home page
+    , which millions of users can watch on their computers and respond.</p> -->
+</body>
+</html>
